2336fa2fcdcdfbedc7da471cfa3b414a5768cdda
maria <mlhc2002@gmail.com>
2024-02-26 18:04:51 +0100

    actualizar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -47,24 +46,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    //ViewModel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    //LiveData
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-
-    // Facilita mvvm en el Fragment
     implementation ("androidx.fragment:fragment-ktx:1.3.2")
-
-    // Facilita mvvm en el Activity
     implementation ("androidx.activity:activity-ktx:1.2.2")
-//retrofit2
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-//Convertirá nuestro Gson a data class. Tendrá anotaciones de Serialización.
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    //Corrutinas
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-
-    //Guide
     implementation ("com.github.bumptech.glide:glide:4.15.1")
 }
